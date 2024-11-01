14e13e04845dacc666663171e89ec71e570f1b1e
RV YASHVANTH <yashvanth27502@gmail.com>
2024-11-01 21:59:30 +0530

    Resume page Updated

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs } from "react-icons/fa";
-import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+import { SiTailwindcss, SiNextdotjs, SiAdobephotoshop, SiAdobeillustrator, SiAdobexd, SiCanva ,SiAdobecreativecloud, SiAdobeindesign, SiAdobeaftereffects, SiFigma   } from "react-icons/si";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@radix-ui/react-tooltip";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
@@ -10,15 +10,15 @@ import { motion } from "framer-motion";
 // About data
 const about = {
   title: 'About me',
-  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo debitis autem dolorem explicabo. Consectetur adipisicing elit.",
+  description: "Hello! I’m Yashvanth, a passionate graphic designer dedicated to crafting elegant digital experiences. With a Bachelor of Engineering (BE) in Computer Science Engineering and over a year of experience in the field, I bring a unique blend of technical skills and creative vision to every project I undertake.",
   info: [
-    { fieldName: "Name", fieldValue: "Yashvanth" },
-    { fieldName: "Phone", fieldValue: "+91 6364427502" },
-    { fieldName: "Experience", fieldValue: "1+ Years" },
-    { fieldName: "Nationality", fieldValue: "Indian" },
-    { fieldName: "Email", fieldValue: "yashvanth27502@gmail.com" },
-    { fieldName: "Freelance", fieldValue: "Available" },
-    { fieldName: "Languages", fieldValue: "English, Kannada, Tamil" },
+    { fieldName: "Name:", fieldValue: "Yashvanth RV" },
+    { fieldName: "Phone:", fieldValue: "+91 6364427502" },
+    { fieldName: "Professional Experience:", fieldValue: "2+ Years" },
+    { fieldName: "Nationality:", fieldValue: "Indian" },
+    { fieldName: "Email:", fieldValue: "yashvanth27502@gmail.com" },
+    { fieldName: "Freelance:", fieldValue: "Available" },
+    { fieldName: "Languages:", fieldValue: "English, Kannada, Tamil" },
   ]
 };
 
@@ -26,11 +26,11 @@ const about = {
 const experience = {
   icon: '/assets/resume/badge.svg',
   title: "My Experience",
-  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel provident deleniti veniam, at, nemo, est saepe odio voluptates sapiente nobis qui rem ea eius quas modi cum. Repudiandae, atque impedit.",
+  description: "With over a year of dedicated experience as a graphic designer, I have developed a strong skill set in creating elegant digital experiences that leave a lasting impression. My journey has allowed me to work on various projects, honing my abilities in logo design and UI design while mastering a variety of design tools and programming languages.",
   items: [
-    { company: "Lorem ipsum dolor sit amet,", position: "GD", duration: "present" },
-    { company: "Lorem ipsum dolor", position: "UIUX", duration: "second job" },
-    { company: "Lorem ipsum", position: "ST", duration: "third job" },
+    { company: "Future Revolution Pvt. Ltd.", position: "Graphic Designer", duration: "2023 - Present" },
+    { company: "Uniferous Pvt. Ltd.", position: "Graphic Designer and UI Designer Intern", duration: "Oct 2023 - Nov 2023" },
+    { company: "Lumos Learning", position: "Software Tester", duration: "Feb 2023 - Sept 2023" },
   ]
 };
 
@@ -38,7 +38,7 @@ const experience = {
 const education = {
   icon: '/assets/resume/badge.svg',
   title: "My Education",
-  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel provident deleniti veniam, at, nemo, est saepe odio voluptates sapiente nobis qui rem ea eius quas modi cum. Repudiandae, atque impedit.",
+  description: "I hold a Bachelor of Engineering (BE) in Computer Science Engineering, which has provided me with a strong technical foundation that complements my work as a graphic designer. While my degree may not directly focus on design, the skills and knowledge I gained during my studies have been invaluable in my creative journey.",
   items: [
     { institution: "K S Institution of Technology", degree: "B.E. in Computer Science of Engineering", duration: "2019 - 2023" },
   ]
@@ -47,15 +47,16 @@ const education = {
 // Skills data
 const skills = {
   title: "My skills",
-  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel provident deleniti veniam, at, nemo, est saepe odio voluptates sapiente nobis qui rem ea eius quas modi cum. Repudiandae, atque impedit.",
+  description: (<>As a <b>Graphic Designer</b> with a focus on creating elegant digital experiences, I possess a diverse skill set that enables me to deliver high-quality design solutions tailored to meet client needs. Here are some of the key skills I bring to the table: </>),
   skillList: [
-    { icon: <FaHtml5 />, name: "HTML 5" },
-    { icon: <FaCss3 />, name: "CSS 3" },
-    { icon: <FaJs />, name: "JavaScript" },
-    { icon: <FaReact />, name: "React JS" },
-    { icon: <SiNextdotjs />, name: "Next.js" },
-    { icon: <SiTailwindcss />, name: "Tailwind CSS" },
-    { icon: <FaFigma />, name: "Figma" },
+    { icon: <SiAdobephotoshop />, name: "Adobe Photoshop" },
+    { icon: <SiAdobeillustrator />, name: "Adobe Illustrator" },
+    { icon: <SiAdobexd />, name: "Adobe XD" },
+    { icon: <SiCanva />, name: "Canva" },
+    { icon: <SiAdobecreativecloud />, name: "Adobe Creative Suit" },
+    { icon: <SiAdobeindesign />, name: "Adobe Indesign" },
+    { icon: <SiAdobeaftereffects  />, name: "Adobe After Effects" },
+    { icon: <SiFigma />, name: "Figma" },
   ]
 };
 
@@ -156,9 +157,9 @@ const Resume = () => {
               <div className="flex flex-col gap-[30px]">
                 <h3 className="text-4xl font-bold">{about.title}</h3>
                 <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{about.description}</p>
-                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0"> 
+                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[780px] mx-auto xl:mx-0"> 
                   {about.info.map((item,index)=>{
-                    return <li key={index} className="flex items-center justify-center xl:justify-start gap-4">
+                    return <li key={index} className="flex items-center justify-center xl:justify-start gap-5">
                       <span className="text-white/60 ">{item.fieldName}</span>
                       <span className="text-xl ">{item.fieldValue}</span>
                     </li>
